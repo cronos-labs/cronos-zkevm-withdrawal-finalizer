f1b5df101df8c6a1e61b749688c9c6d8143792b4
Ivan SL Wong <ivan.sl.wong@crypto.com>
2024-01-09 17:28:25 +0800

    Edited nix flake

--- flake.nix
+++ flake.nix
@@ -2,74 +2,61 @@
   inputs.crane.url = "github:ipetkov/crane";
   inputs.fenix.url = "github:nix-community/fenix";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.std.url = "github:divnix/std";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
   outputs = {
     crane,
     fenix,
     flake-utils,
     nixpkgs,
-    self,
-    std,
     ...
-  } @ inputs: let
-    systems = ["x86_64-linux"];
-    inputs' =
-      inputs
-      // (flake-utils.lib.eachSystem systems (system: let
-        pkgs = nixpkgs.legacyPackages.${system};
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
 
-        inherit (pkgs.lib) cleanSourceWith;
+      inherit (pkgs.lib) cleanSourceWith;
 
-        toolchain = with fenix.packages.${system};
-          combine [
-            default.cargo
-            default.rustc
-          ];
+      toolchain = with fenix.packages.${system};
+        combine [
+          default.rustc
+          default.cargo
+        ];
+      craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
 
-        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+      src = cleanSourceWith {
+        src = ./.;
+        filter = _: _: true;
+      };
 
-        src = cleanSourceWith {
-          src = ./.;
-          filter = _: _: true;
-        };
+      commonArgs = with pkgs; {
+        inherit src;
 
-        commonArgs = with nixpkgs; {
-          inherit src;
+        strictDeps = true;
 
-          strictDeps = true;
-          cargoVendorDir = craneLib.vendorMultipleCargoDeps {
-            inherit (craneLib.findCargoFiles src) cargoConfigs;
-            cargoLockList = [
-              ./Cargo.lock
+        cargoLock = ./Cargo.lock;
+        cargoToml = ./Cargo.toml;
 
-              "${fenix.packages.${system}.complete.rust-src}/lib/rustlib/src/rust/Cargo.lock"
-            ];
-          };
-        };
+        buildInputs = [
+          openssl
+        ];
+        nativeBuildInputs = [pkg-config];
 
-        cargoArtifacts = craneLib.buildDepsOnly (commonArgs
-          // {
-          });
+        BINDGEN_EXTRA_CLANG_ARGS = ''-I"${libclang.lib}/lib/clang/16/include"'';
+        LIBCLANG_PATH = lib.makeLibraryPath [libclang.lib];
+      };
 
-        my-crate = craneLib.buildPackage (commonArgs
-          // {
-            inherit cargoArtifacts;
-            cargoExtraArgs = "--bin withdrawal-finalizer";
-          });
-      in {inherit my-crate;}));
-  in
-    std.growOn {
-      inherit systems;
-      inputs = inputs';
-      cellsFrom = ./nix;
-      cellBlocks = with std.blockTypes; [
-        #(containers "oci-images" {ci.publish = true;})
-        (installables "packages" {ci.build = true;})
-        #(runnables "operables")
-      ];
-    } {
-      packages = std.harvest self ["local" "packages"];
-    };
+      cargoArtifacts = craneLib.buildDepsOnly (commonArgs
+        // {
+        });
+      
+      withdrawal-finalizer = craneLib.buildPackage (commonArgs
+        // {
+          inherit cargoArtifacts;
+
+          cargoExtraArgs = "--bin withdrawal-finalizer";
+        });
+
+    in {
+      packages.withdrawal-finalizer = withdrawal-finalizer;
+    });
 }
